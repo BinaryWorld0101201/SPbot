62e5a2ae57ce86024bf7f908dffcd11bcf574518
Lenart8274 <44343581+Lenart8274@users.noreply.github.com>
2018-11-29 01:06:24 +0100

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,9 @@
+<?php
+
+$leggi_messaggi_in_uscita = true;
+
+$lista_admin = [
+  40955937,  //id di Bruno :D
+  101374607,  //id del creatore di MadelineProto :D
+  771511654,  //un id probabilmente inesistente
+];
